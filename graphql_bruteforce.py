fe895f7af587baa9e5c6b460b47e2f044034897c
hunjison <esby97@naver.com>
2021-02-13 22:36:59 +0900

    First Commit

---
+++ graphql_bruteforce.py
@@ -0,0 +1,74 @@
+import setting as s
+import make_wordlist
+import re
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from collections import defaultdict
+
+
+# Disable flag warning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning) 
+
+# regex
+p = re.compile('(?<=Did you mean \\\\")[\w_]{1,20}(?=\\\\")')
+p2 = re.compile('(?<=Did you mean \\\\")[\w_]{1,20}.{1,5} or .{1,5}[\w_]{1,20}(?=\\\\")')
+p3 = re.compile('(?<=must not have a selection since type \\\\")[\w_]{1,20}(?=\\\\" has)')
+
+
+def bruteforce(context=[]):
+    # default setting
+    find = dict()
+    data = s.csv2list(s.wordlist_PATH)
+    wordlist = [x for sublist in data for x in sublist] # less then 10000
+
+    # query setting
+    c = ['']+ context
+    query_left = '{'.join(c) + '{'
+    query_right = '}'*len(c)
+    query = " ".join(wordlist)
+    data = r'{"query":"'+ query_left+ query + query_right + '"}'
+
+    # request send
+    res = requests.post(url=s.url, data=data, headers=s.headers, verify=False)
+    m = p.findall(res.text)
+    m2 = p2.findall(res.text)
+    m3 = p3.findall(res.text)
+    if m:
+        find.update(dict.fromkeys(m, ''))
+    if m2:
+        _m2 = []
+        for x in m2:
+            _m2 += x.split('\\" or \\"')
+        find.update(dict.fromkeys(_m2, ''))
+    if m3:
+        find = m3[0] # Type ( e.g. String )
+
+    return find
+
+
+# BFS
+visit = list()
+queue = list()
+context = []
+output = defaultdict(dict)
+queue.append(context)
+print("[+] Start!")
+
+while queue: # context queue, each node means each context.
+    print("[*] queue left : ", len(queue), "..", queue[-2:])
+    node = queue.pop(0)
+    if node not in visit:
+        visit.append(node)
+        find = bruteforce(node)
+        if find:
+            output = s.context2dict(output, node, find)
+            if type(find) == type(''):
+                print("[+] Find Type : ", node, find)
+            elif len(node) <= s.depth:
+                queue.extend([node + [x] for x in list(find.keys())])
+            else:
+                print("[-] Depth Over.. I will not anaylize", node) # pass if depth is over
+        s.dict2json(output, s.output_PATH)
+
+print("[+] finished!!")
+
